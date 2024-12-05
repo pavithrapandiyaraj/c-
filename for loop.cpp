973b30cc8cab5c229c91f45c18bec27e6700d130
pavithrapandiyaraj <144935069+pavithrapandiyaraj@users.noreply.github.com>
2024-12-05 14:33:02 +0530

    Add files via upload
    Write a program which takes two values x and y. Prints x for y number of times.

---
+++ for loop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int x;
+	int y;
+	cin>> x >> y;
+	for(int i=1 ; i<=y; i++){
+	    cout << x << endl;
+	}
+
+    return 0;
+}
