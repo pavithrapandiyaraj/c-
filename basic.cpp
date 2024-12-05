9a4d1e807a6cf321dd9ef132c063371379daec55
pavithrapandiyaraj <144935069+pavithrapandiyaraj@users.noreply.github.com>
2024-12-05 12:52:03 +0530

    Add files via upload
    Prob 1 : To Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.

---
+++ basic.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+	char let;
+	string word;
+	cin>> a;
+	cin>> let;
+	cin>> word;
+	cout<< a << endl;
+	cout<< let << endl;
+	cout<< word << endl;
+	return 0;
+
+}
