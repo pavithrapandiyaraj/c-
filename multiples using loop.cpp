0a29e69bb5a92a1c35e2fdb17b8d86d115dad2e9
pavithrapandiyaraj <144935069+pavithrapandiyaraj@users.noreply.github.com>
2024-12-05 14:42:28 +0530

    Add files via upload
    Write a program to take x and print multiples of x till 1000

---
+++ multiples using loop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int x;
+	cin>> x;
+	for(int i=1; i<=1000; i++){
+	    cout<< i*x << endl;
+	}
+
+    return 0;
+}
